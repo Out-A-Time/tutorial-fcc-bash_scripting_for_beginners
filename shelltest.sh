3c3bdb124187c834361d47d868b544670a7f84ca
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2023-05-30 23:00:51 +0100

    Positional variables example added to script

--- shelltest.sh
+++ shelltest.sh
@@ -4,9 +4,14 @@ echo Hello Max
 
 FIRST_NAME=Max
 LAST_NAME=Kolonko
-echo "Hello $FIRST_NAME $LAST_NAME"
+echo Hello $FIRST_NAME $LAST_NAME
 #Overwriting variable
 echo What is your name?
 read FIRST_NAME
 
 echo "Hello $FIRST_NAME $LAST_NAME"
+
+#Positional variables run it with  command 1 2 3
+echo "I firmly believe that $1 is the best  $2 for the office of $3"
+
+
